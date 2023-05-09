07540e754a263b6cf908d8a3d7624059548434f6
kiotoks <nikogr2005@gmail.com>
2023-05-09 09:48:41 -0300

    Josue Bieber

---
+++ breakfast/Desayuno.java
@@ -0,0 +1,42 @@
+package breakfast;
+
+public class Desayuno {
+    private String[]ingredientes;
+    private double precio;
+    private int num;
+    
+            
+    public Desayuno(String[]i, double p, int n) {
+       this.ingredientes = i;
+       this.precio = p;
+       this.num =n;
+       
+       if(num == 11){
+           precio = 0;
+       }
+    }
+
+    public String[] getIngrediente() {
+        return ingredientes;
+    }
+
+    public void setIngrediente(String[] ingrediente) {
+        this.ingredientes = ingrediente;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public String mostrarDesayuno(){
+        String datos = "ingredeientes: ";
+        for (String ingrediente : ingredientes) {
+            datos += ingrediente + ", ";
+        }
+        datos += "precio: "+ precio;
+        return datos;
+    } 
+}
